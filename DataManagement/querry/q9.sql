340c43b49bfe07a7a37ce41c175835dd993eb80b
SLakehal27 <lakehalselim27@gmail.com>
2026-01-27 21:06:44 -0500

    Q9 attempt

--- DataManagement/querry/q9.sql
+++ DataManagement/querry/q9.sql
@@ -1,2 +1,4 @@
 -- Classer en ordre décroissant de note et de nombre de personnage pour les consoles qui ont la meuilleure note moyenne pour les jeux sortis.
-
+-- There was an attempt lol --
+SELECT console.nom FROM console CROSS JOIN sortie CROSS JOIN personnage NATURAL JOIN jeu 
+ORDER BY jeu.note DESC, personnage.nom
